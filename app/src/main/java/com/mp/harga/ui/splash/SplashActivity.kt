7ee7ca3f265fef14273f962acded97402b69a337
abdulkharisismail <abdulkharisismail97@gmail.com>
2022-08-17 00:03:47 +0700

    Add splash animation

--- app/src/main/java/com/mp/harga/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/mp/harga/ui/splash/SplashActivity.kt
@@ -5,7 +5,9 @@ import android.os.Handler
 import android.os.Looper
 import android.view.View
 import android.view.WindowManager
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatDelegate
+import com.mp.harga.R
 import com.mp.harga.databinding.ActivitySplashBinding
 import com.mp.harga.ui.base.BaseActivity
 import com.mp.harga.utils.contant.Constant
@@ -13,10 +15,11 @@ import com.mp.harga.utils.extensions.goToMain
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : BaseActivity() {
-    private lateinit var binding: ActivitySplashBinding
+    private var _binding: ActivitySplashBinding? = null
+    private val binding get() = _binding!!
 
     override fun initLayout(): View {
-        binding = ActivitySplashBinding.inflate(layoutInflater)
+        _binding = ActivitySplashBinding.inflate(layoutInflater)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         return binding.root
     }
@@ -24,10 +27,17 @@ class SplashActivity : BaseActivity() {
     override fun initView() {
         super.initView()
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        val animation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        binding.llLogoApps.animation = animation
         Handler(Looper.getMainLooper()).postDelayed({
             goToMain()
             finish()
         }, Constant.SPLASH_TIME_OUT)
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
